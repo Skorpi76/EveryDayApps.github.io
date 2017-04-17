a2a3df59133b64c23957597d92afc23dff63f373
Vadym Harkusha <vharkush@my.centennialcollege.ca>
2017-04-17 20:13:16 -0300

    Styles updated for snake game page

--- Entertainment/SnakeGame/Snake.js
+++ Entertainment/SnakeGame/Snake.js
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        canvas {
-            display: block;
-            position: absolute;
-            border: 1px solid #000;
-            margin: auto;
-            top: 0;
-            bottom: 0;
-            right: 0;
-            left: 0;
-        }
-    </style>
-</head>
-
-<body>
-    <script>
         //Eskender Memetov
 
         // Constants
@@ -227,7 +203,3 @@
 
 
         main();
-    </script>
-</body>
-
-</html>
